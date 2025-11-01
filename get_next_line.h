204e8a881a5f4a080a75a543cb6c45a38c59ee96
Maurice Lorenz <me@mauricelorenz.com>
2025-11-01 22:03:21 +0100

    Add BUFFER_SIZE macro to get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,20 @@
 /*   By: mlorenz <mlorenz@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 22:40:11 by mlorenz           #+#    #+#             */
-/*   Updated: 2025/10/30 22:47:35 by mlorenz          ###   ########.fr       */
+/*   Updated: 2025/11/01 21:23:09 by mlorenz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
 # include <stdlib.h>
 # include <unistd.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
 char	*get_next_line(int fd);
+
 #endif
