a4d4ecc61db4c7450f42524a75c00b74d1b4a11a
Maurice Lorenz <me@mauricelorenz.com>
2025-11-08 16:36:02 +0100

    Finalize helper functions create_or_grow_str() and str_append()

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,33 +6,49 @@
 /*   By: mlorenz <mlorenz@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 13:37:57 by mlorenz           #+#    #+#             */
-/*   Updated: 2025/11/08 14:55:31 by mlorenz          ###   ########.fr       */
+/*   Updated: 2025/11/08 16:30:00 by mlorenz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	create_or_grow_str(char **old_str, char *to_add)
+int	create_or_grow_str(char **old_str, char *to_add, char to_add_end)
 {
-	char	*old_str_orig;
+	char	*old_str_ptr;
 	char	*new_str;
-	char	*new_str_orig;
+	char	*new_str_ptr;
 
-	old_str_orig = *old_str;
+	old_str_ptr = *old_str;
 	new_str = malloc(len_to_char(*old_str, '\0')
-			+ len_to_char(to_add, '\n') + 1);
+			+ len_to_char(to_add, to_add_end) + 1);
 	if (!new_str)
 		return (1);
-	new_str_orig = new_str;
+	new_str_ptr = new_str;
 	if (*old_str)
 	{
-		while (**old_str)
-			*new_str++ = *(*old_str)++;
-		free(old_str_orig);
+		while (*old_str_ptr)
+			*new_str_ptr++ = *old_str_ptr++;
+		free(*old_str);
 	}
-	*new_str = '\0';
-	*old_str = new_str_orig;
+	*new_str_ptr = '\0';
+	*old_str = new_str;
 	return (0);
+}
+
+void	str_append(char **str, char **to_add, char to_add_end)
+{
+	size_t	i;
+	size_t	len_to_add;
+	char	*str_ptr;
+
+	i = 0;
+	len_to_add = len_to_char(*to_add, to_add_end);
+	str_ptr = *str;
+	while (*str_ptr)
+		str_ptr++;
+	while (i++ < len_to_add)
+		*str_ptr++ = *(*to_add)++;
+	*str_ptr = '\0';
 }
 
 size_t	len_to_char(char *s, char c)
