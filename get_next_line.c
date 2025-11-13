e3e99a198bd0ee465f011d8e2be1f248fc7611e6
Maurice Lorenz <me@mauricelorenz.com>
2025-11-13 17:03:08 +0100

    Add main logic in pseudocode to get_next_line() and add helper functions

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mlorenz <mlorenz@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 13:00:21 by mlorenz           #+#    #+#             */
-/*   Updated: 2025/11/13 13:00:24 by mlorenz          ###   ########.fr       */
+/*   Updated: 2025/11/13 14:19:12 by mlorenz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,40 @@
 
 char	*get_next_line(int fd)
 {
-	
+	char		*line;
+	char		*buf;
+	static char	*rest;
+	size_t		tmp_n;
+	char		*tmp_ptr;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	line = NULL;
+	buf = NULL;
+	if (rest)
+	{
+		if (ft_strchr(rest, '\n'))
+		{
+			// copy rest till \n to line
+			// set rest to index \n + 1 and free old rest
+			// return line
+		}
+		else
+		{
+			// copy rest till \0 to line
+			// free rest
+		}
+	}
+	while (1)
+	{
+		// read to buf
+		// if read size = 0 return 0
+		// if read_size < BUFFER_SIZE copy buf to line and return line
+		if (ft_strchr(buf, '\n'))
+		{
+			// copy buf till \n to line
+			// copy buf to rest at index \n + 1 and free buf
+			// return line
+		}
+	}
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,9 +6,57 @@
 /*   By: mlorenz <mlorenz@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 13:00:27 by mlorenz           #+#    #+#             */
-/*   Updated: 2025/11/13 13:00:30 by mlorenz          ###   ########.fr       */
+/*   Updated: 2025/11/13 13:28:56 by mlorenz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if ((unsigned char)*s == (unsigned char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((unsigned char)c == '\0')
+		return ((char *)s);
+	return (0);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (*s++)
+		i++;
+	return (i);
+}
+
+size_t	ft_nllen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (*s++ != '\n')
+		i++;
+	return (i + 1);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+	char	*char_dest;
+	char	*char_src;
+
+	i = 0;
+	char_dest = (char *)dest;
+	char_src = (char *)src;
+	if (!dest && !src)
+		return (0);
+	while (i++ < n)
+		*char_dest++ = *char_src++;
+	return (dest);
+}
